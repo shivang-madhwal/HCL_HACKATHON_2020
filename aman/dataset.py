fc9df2690aaa2f7e80c309b71b3e34133c9ffefe
amanyadav222 <amansy04@gmail.com>
2020-06-07 20:38:34 +0530

    ticket number added

--- aman/dataset.py
+++ aman/dataset.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('events.csv')
+df = pd.read_csv('transactionFinal.csv')
 print(len(df.itemid.unique()))
-trans = df[df['event']=='transaction']
+# trans = df[df['event']=='transaction']
 
-trans.to_csv('transaction.csv')
+# trans.to_csv('transaction.csv')
 
-df1 = pd.read_csv('transaction1.csv')
-print(len(df1.no.unique()))
+# df1 = pd.read_csv('transaction1.csv')
+# print(len(df1.no.unique()))
